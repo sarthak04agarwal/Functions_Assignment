db64a09b2a32ee03aa86e4abf702704d849c8885
sarthak04agarwal <sarthak04agarwal@gmail.com>
2022-10-31 22:28:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 test = [2, 4, 6, 8, 10]    
+test1 = [2, 5, 10, 20, 21]
 
 def contains(aList, item):
-    running = True
+    if item in aList:
+        return( str(item) + " is IN the list")
+    else:
+        return( str(item) + " is NOT in the list")
 
+
+'''
 while(running):
     for x in range(0, len(test)):
         if (item == aList[x]):
@@ -10,9 +16,8 @@ while(running):
             print( str(item) + " is IN the list")
     else:
         print( str(item) + " is NOT in the list")
-    
+'''
 
-aList = input("Please enter the name of the list: ")
-item = input("Please enter the item that you want to search inside the list: ")
-
-contains()
+list_name = input("Please enter the name of the list that you want to check: ")
+item_List = input("Please enter the item that you want to search inside the list: ")
+print(contains(list_name, item_List))
